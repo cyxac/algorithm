484ee0fd0f38d090f890dedbea95c42851bd3089
Zanqi <zanqi.liang@gmail.com>
2013-01-06 14:53:24 -0500

    Refactor and add comment

--- 145/HillHike.rb
+++ 145/HillHike.rb
@@ -1,29 +1,24 @@
 def num_paths distance, max_height, landmarks
-    cache1 = Array.new(2) { Array.new(52) { Array.new(51) { 0 } } }
-    cache2 = Array.new(cache1)
-    cache1[0][0][0] = 1
-    1.upto(distance - 1) do |i|
-       cache2 =  Array.new(2) { Array.new(52) { Array.new(51) { 0 } } }
-       1.upto(max_height) do |j|
-           seen_max = j == max_height ? 1 : 0
+    cache1 = Hash.new(0) # Array.new(2) { Array.new(52) { Array.new(51) { 0 } } }
+    cache1[[false,0,0]] = 1 # Treat it as 3d array. indexed by <seen_max, height, num_landmarks>
+    1.upto(distance - 1) do
+       cache2 = Hash.new(0)
+       1.upto(max_height) do |h|
+           max = h == max_height
            0.upto(landmarks.size) do |k|
                -1.upto(1) do |d|
-                   lm = j == landmarks[k] ? k + 1 : k
-                   # if j == max_height
-                   #     cache2[1][j][lm] += cache1[0][j + d][k]
-                   #     cache2[1][j][lm] += cache1[1][j + d][k]
-                   # else
-                   #     cache2[0][j][lm] += cache1[0][j + d][k]
-                   #     cache2[1][j][lm] += cache1[1][j + d][k]
-                   # end
-                   cache2[seen_max][j][lm] += cache1[0][j + d][k]
-                   cache2[1][j][lm] += cache1[1][j + d][k]
+                   lm = h == landmarks[k] ? k + 1 : k
+                   # if h != max_height, update both seen max and not seen max table,
+                   # otherwise, use sum of the true and false data to update the true data,
+                   # false data should be zero at h, so leave it to the hash.
+                   cache2[[max,h,lm]] += cache1[[false,h + d,k]]
+                   cache2[[true,h,lm]] += cache1[[true,h + d,k]]
                end
            end
        end
        cache1 = cache2
     end
-    return cache1[1][1][landmarks.size]
+    return cache1[[true,1,landmarks.size]]
 end
 
 puts num_paths(5, 2, []) # 3
