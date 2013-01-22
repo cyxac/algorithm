dedc13bf4cb475e365f41ec3b11c28222a6933e6
Zanqi <zanqi.liang@gmail.com>
2013-01-21 19:14:11 -0500

    lecture code

---
+++ 6006/radixSort.rb
@@ -0,0 +1,22 @@
+def counting_sort(input, k)
+    c = [0]*k
+    input.each do |e|
+        c[e] += 1
+    end
+    1.upto(c.size-1) do |i|
+        c[i] += c[i-1]
+    end
+    output = [nil]*input.size
+    input.reverse_each do |e|
+        output[c[e]-1] = e
+        c[e] -= 1
+    end
+    return output
+end
+
+def radix_sort(A, d)
+#    for i in 1 to d
+#        use a stable sort to sort array A on digit i
+end
+
+p counting_sort [5,1,4,2,3],6
