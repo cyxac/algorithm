80eff245e4c4ff2e81a504fef0a8d39a40ed3228
Zanqi <zanqi.liang@gmail.com>
2013-01-26 22:17:36 -0500

    rolling hash

---
+++ 6006/RollingHash.rb
@@ -0,0 +1,24 @@
+class RollingHash
+    def initialize base
+        @base = base
+        @hash = 0
+        @magic = 1
+        @p = 23 #random large prime >= size of pattern, but less than word size
+        @inverse_base = @base**(@p-2) mod p
+    end
+    
+    def hash
+        @hash
+    end
+    
+    def append val
+        @hash = (@hash*base + val) % @p
+        @magic = @magic * @base % @p
+    end
+    
+    def skip val
+        @hash = (@hash - @magic*val) % @p
+        @magic = @magic * @inverse_base % p
+        # modulo arithmethic for: @magic = @magic / @base % @p
+    end
+end
