a9430e142614bc1d3ae0fe320e9dfe36ea57cc9f
Zanqi Liang <zanqi.liang@gmail.com>
2013-08-05 13:03:32 -0400

    Rename

--- 97si/math97.rb
+++ 97si/math97.rb
@@ -1,8 +1,23 @@
+require "matrix"
+include Math
+
 class Integer
   # binomial coefficient: n C k
   def choose(k)
     # n!/(n-k)! / k!
     (self-k+1 .. self).reduce(1, :*) / (2 .. k).reduce(1, :*)
+  end
+end
+
+class Vector
+  def rotate(angle)
+    Matrix[[cos(angle), -sin(angle)], [sin(angle), cos(angle)]] * self
+  end
+end
+
+class Array
+  def rotate(angle)
+    Vector.elements(self).rotate(angle).to_a
   end
 end
 
@@ -16,12 +31,16 @@ module Math97
     end
     ret
   end
+
+  module_function :pow
 end
 
 module Test
-  def self.foo
+  def foo
     p 'yeah'
   end
+
+  module_function :foo
 end
 
 if __FILE__ == $0
@@ -34,6 +53,9 @@ if __FILE__ == $0
   p pow 5, 0
   p pow 5, 10
   
-  include Math
-  Math.cos
+  Test.foo
+
+  p Vector[1, 0].rotate(PI/2)
+
+  p [1,0].rotate(PI/2)
 end
