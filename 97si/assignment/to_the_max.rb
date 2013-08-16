2f5c31f5b0c48de62caccf5f2fcbce59008fe0a1
Zanqi Liang <zanqi.liang@gmail.com>
2013-08-16 11:25:54 -0400

    dp

--- 97si/assignment/to_the_max.rb
+++ 97si/assignment/to_the_max.rb
@@ -1,24 +1,44 @@
 require 'stringio'
 
+def max_sub_array a
+    max_sub = []
+    max = -Float::INFINITY
+    t_max_sub = []
+    t_sum = -Float::INFINITY
+    
+    a.each_with_index do |e, i|
+        if t_sum <= 0
+            t_sum = e
+            t_max_sub = [i, i]
+        else
+            t_sum += e
+            t_max_sub[1] = i
+        end
+        
+        if max < t_sum
+            max_sub = t_max_sub.dup
+            max = t_sum
+        end
+    end
+    [max, a[max_sub[0]..max_sub[1]]]
+end
+
 def solve inputstr
   @input = StringIO.new inputstr
   n = @input.gets.to_i
-  ints = @input.read.split.map(&:to_i)
-  a = Array.new(n) do |i|
-    Array.new(n) do |j|
-      ints[i*n + j]
+  a = @input.read.split.map(&:to_i)
+  max = a[0][0]
+  a = Array.new(n) { |i| Array.new(n) { |j| a[i*n+j] } }
+  a.each_index { |i| 
+    t_max = max_sub_array(a[i])[0]
+    max = t_max if t_max > max
+    (i+1).upto n-1 do |j|
+      a[i] = a[i].zip(a[j]).map { |pair| pair[0]+pair[1] }
+      t_max = max_sub_array(a[i])[0]
+      max = t_max if t_max > max
     end
-  end
-  # dp = Array.new(n) { Array.new(n) }
-  # dp[0][0] = [a[0][0], 1, 1]
-  # 0.upto n-1 do |i|
-  #   0.upto n-1 do |j|
-  #     next if i == 0 && j == 0
-  #     adj = [[a[i][j], 1, 1]]
-
-
-  #   end
-  # end
+  }
+  max
 end
 
 str = 
@@ -28,7 +48,4 @@ str =
 
 8 0 -2"
 
-# p solve str
-
-a = Array.new(4) { Array.new(4) { rand(-10..10) } }
-a.each { |e| p e }
+p solve str
