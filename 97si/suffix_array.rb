8c0ca958e6348ee375b5d3bdaa87de1b4fdfaedf
Zanqi <zanqi.liang@gmail.com>
2013-09-14 14:00:32 -0400

    sa

---
+++ 97si/suffix_array.rb
@@ -0,0 +1,25 @@
+# Construction in O(nlog^2(n)) time
+def get_suffix_array t
+  l = t.size
+  p = [[]]
+  0.upto t.size-1 do |i| p[0][i] = t[i].ord end
+  skip, level = 1, 1
+  while skip < l
+    p << []
+    m = l.times.map do |i|
+      [[p[level-1][i], i+skip < l ? p[level-1][i+skip] : -1000], i]
+    end
+    m.sort!
+    l.times do |i|
+      p[level][m[i][1]] = i>0 && m[i][0]==m[i-1][0] ? p[level][m[i-1][1]] : i
+    end
+    skip *= 2
+    level += 1
+  end
+  p[-1]
+end
+
+if __FILE__ == $0
+  sa = get_suffix_array "bobocel"
+  p sa
+end
